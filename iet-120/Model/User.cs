51829f43d229141f90683823dd141dff4e6a8965
Vinicius Pontes <vinicius.pontespereira@swisscom.com>
2019-08-13 17:55:24 +0200

    Added Models and Migrations

---
+++ iet-120/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iet_120.Model
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Salutation { get; set; }
+        [Required]
+        [Display(Name = "Vorname")]
+        public string Firstname { get; set; }
+        [Required]
+        [Display(Name = "Nachname")]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
---
+++ iet-120/Model/Word.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iet_120.Model
+{
+    public class Word
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
